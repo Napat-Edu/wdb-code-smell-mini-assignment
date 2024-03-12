48720ead08134cd0a70de825a2246555e65cdb82
Napat-Edu <omegyzr@gmail.com>
2024-03-12 19:59:39 +0700

    add test for total func

--- tests/total.test.js
+++ tests/total.test.js
@@ -1,9 +1,21 @@
-const total = require('../total')
+const total = require("../total");
 /* test('total of empty string should be 0', () => {
     expect(total('')).toBe(0);
 }) */
-describe('Total Test', () => {
-    it('should test that true === true', () => {
-        expect(true).toBe(true)
-    })
-})
+describe("Total Test", () => {
+  it("should return 3 if 1,2", () => {
+    expect(total("1,2")).toBe(3);
+  });
+
+  it("should return 4 if 2,2", () => {
+    expect(total("2,2")).toBe(4);
+  });
+
+  it("should return 0 if empty", () => {
+    expect(total("")).toBe(0);
+  });
+
+  it("should return ?", () => {
+    expect(total("1,3000")).toBe(0);
+  });
+});
